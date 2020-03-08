8072a4487d8b952e39e5d60c47850df2befaba5c
Deb Jones <debjones513@gmail.com>
2020-03-08 16:49:38 -0700

    Adding functions and namespaces

--- src/hello/core.clj
+++ src/hello/core.clj
@@ -11,11 +11,14 @@
   (println "Hello, World!!!")
 
   (hello.firstns/greet "deb")
-  (hello.firstns/greet2 "Gary")
+  (hello.firstns/greet2 "jones")
   (hello.firstns/greet3 "Klay")
   (hello.firstns/def_a_str)
   (hello.firstns/do_nothing 8)
 
   (hello.secondns/ns_2_fn1 99)
+  (hello.secondns/all-things 99 98 97)
+  (hello.secondns/all-things)
+  (hello.secondns/apply_something)
   )
 
--- src/hello/secondns.clj
+++ src/hello/secondns.clj
@@ -5,3 +5,20 @@
   [input]
   (println "Printing input: ", input)
   )
+
+(defn all-things [& x]
+  ;; Takes a variable number of args
+  (println x)
+  )
+
+(defn apply_something
+  ;; Apply a core fn to a set of params
+  []
+  (def x (list 7 8 9))
+  (println "Apply + to list x" (apply + x))
+
+  (println "Apply max " (apply max [1 3 2]))                ;; You have to apply max to get max in the vector.
+  (println "Max "  (max [1 3 2]))                           ;; max has one arg and it is a vector
+  (println "Apply + to vector"  (apply + [1 2 3]))
+  (println "Apply + to list" (apply + (list 4 5 6)))
+  )
